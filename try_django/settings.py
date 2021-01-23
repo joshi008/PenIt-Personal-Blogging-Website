d2c61159f49634f8dfd4cfc98ad4973c8b7a3aae
Hrishabh Joshi <joshi008@users.noreply.github.com>
2021-01-23 12:14:05 +0530

    Update Django for database

--- try_django/settings.py
+++ try_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'jfqw$ev6c#l$#l#4p*-30ez902cgbzw#52*&#0d=l18zq@u#4('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['penit-blog.herokuapp.com','*']
 
 LOGIN_URL = '/login'
 
@@ -90,9 +90,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'CONN_MAX_AGE': 500
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
